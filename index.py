95ca69ba7485534e774ff8a6bffa322510795e36
mbrsagor <mbrsagor@gmail.com>
2021-05-27 23:42:56 +0600

    New: Build a Webcam Face Detector

--- index.py
+++ index.py
@@ -1,33 +1,22 @@
-from imutils import paths
-import face_recognition
-import pickle
 import cv2
 import os
- 
-#get paths of each file in folder named Images
-#Images here contains my data(folders of various persons)
-imagePaths = list(paths.list_images('Images'))
-knownEncodings = []
-knownNames = []
-# loop over the image paths
-for (i, imagePath) in enumerate(imagePaths):
-    # extract the person name from the image path
-    name = imagePath.split(os.path.sep)[-2]
-    # load the input image and convert it from BGR (OpenCV ordering)
-    # to dlib ordering (RGB)
-    image = cv2.imread(imagePath)
-    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    #Use Face_recognition to locate faces
-    boxes = face_recognition.face_locations(rgb,model='hog')
-    # compute the facial embedding for the face
-    encodings = face_recognition.face_encodings(rgb, boxes)
-    # loop over the encodings
-    for encoding in encodings:
-        knownEncodings.append(encoding)
-        knownNames.append(name)
-#save emcodings along with their names in dictionary data
-data = {"encodings": knownEncodings, "names": knownNames}
-#use pickle to save data into a file for later use
-f = open("face_enc", "wb")
-f.write(pickle.dumps(data))
-f.close()
+
+# Using the Haar Cascade from OpenCV
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+video = cv2.VideoCapture(0)
+
+while True:
+    check, frame = video.read()
+    faces = face_cascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5)
+    for x, y, w, h in faces:
+        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    cv2.imshow('Face Detector', frame)
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
