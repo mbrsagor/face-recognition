2f359c3afb14be965f8de1a6abdf445aaa8a2ac8
mbrsagor <mbrsagor@gmail.com>
2022-03-19 23:16:47 +0600

    New: Product delete udpate API

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.encoders import jsonable_encoder
+
 from redis_om import get_redis_connection, HashModel
 
 app = FastAPI()
@@ -54,3 +56,23 @@ def format(pk: str):
 @app.post('/create-product/')
 def create_product(product: Product):
     return product.save()
+
+
+@app.get('/product/{pk}/')
+def product_details(pk: str):
+    try:
+        return Product.get(pk)
+    except Exception as e:
+        return str(f"The product ID not found{e}")
+
+
+@app.put("/product/{pk}", response_model=Product)
+async def update_product(pk: str, product: Product):
+    update_item = jsonable_encoder(product)
+    product[pk] = update_item
+    return update_item
+
+
+@app.delete('/product/{pk}/')
+def delete_product(pk: str):
+    return Product.delete(pk)
