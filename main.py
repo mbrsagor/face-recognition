aa9c17905d0e7bbcb3eb45ba886c5895cae1bd02
mbrsagor <mbrsagor@gmail.com>
2023-09-10 10:28:27 +0600

    New: project configuration and db crated done.

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Item(BaseModel):  # Serializer
+    id: int
+    name: str
+    price: int
+    on_offer: bool
+    description: str
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get('/user/{name}')
+def call_name(name: str):
+    return {'message': f"Hello Mr. {name}"}
+
+
+@app.put('/item/{item_id}/')
+def update_item(item_id: int, item: Item):
+    return {
+        'name': item.name,
+        'price': item.price,
+        'on_offer': item.on_offer,
+        'description': item.description
+    }
