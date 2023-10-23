d26a695652192c82e8c1413b8127d6651c5f2243
mbrsagor <mbrsagor@gmail.com>
2023-10-23 10:49:35 +0600

    Remove: user services removed.

--- models/models.py
+++ models/models.py
@@ -1,41 +1,9 @@
 import uuid
-import jwt
-import bcrypt
-from utils.key import SECRET_KEY
-from datetime import datetime, timedelta
 
 from database.database import BASE
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy import Column, String, Integer, Boolean, Text, ForeignKey
-
-
-class User(BASE):
-    __tablename__ = 'users'
-
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    posts = relationship('Posts', back_populates='author')
-
-    def hash_password(self, password: str):
-        self.hashed_password = bcrypt.hashpw(password.encode('utf-8'))
-        bcrypt.gensalt().decode('utf-8')
-
-    def verity_password(self, password: str):
-        return bcrypt.checkpw(password.encode('utf-8'), self.hash_password('utf-8'))
-
-    def generate_token(self):
-        expiration = datetime.utcnow() + timedelta(hours=24)
-        payload = {
-            "sub": str(self.id),
-            "exp": expiration
-        }
-        return jwt.encode(payload, SECRET_KEY, algorithm='H256')
-
-    def __repr__(self):
-        return self.username
 
 
 class Category(BASE):
