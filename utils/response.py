43d484ba9058cc7a809a21bad7c119efe10cd4cd
mbrsagor <mbrsagor@gmail.com>
2024-08-02 17:32:13 +0600

    New: Added responses.

--- utils/response.py
+++ utils/response.py
@@ -1,3 +1,24 @@
+from utils import messages
+
+
+def prepare_create_success_response(message):
+    """ prepare success response for all serializer """
+    response = {
+        'status': 'success',
+        'message': message
+    }
+    return response
+
+
+def prepare_error_response(serializer_error):
+    """ prepare error response for all serializer """
+    response = {
+        'status': 'fail',
+        'message': serializer_error,
+    }
+    return response
+
+
 def data_deleted():
     return {
         'status': True,
